c83a75807812ca17f81965f09cb75e7c7d4e4cab
Server <november14giel@gmail.com>
2018-05-07 12:56:07 -0400

    added bussiness card content

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,26 @@ var dealers = require('../data/dealers')
 router.get("/", function(req, res, next) {
  
     var poolPros = []; //were gonna render this array, in our HOOK, as a key in our hbs page
+    var saturday =[];
     var chunksPerRow = 3; //just to say, we want each row to contain three cards/container
+    var sat = poolPros.data;
     //here we loop through dealers
+    for (let i = 0; i < dealers.length; i++) {
+     saturday.push(dealers[i].data.weekHours.sat);
+    }
     for (var i = 0; i < dealers.length; i += chunksPerRow) {
       // i is incremented by three
       poolPros.push(dealers.slice(i, i + chunksPerRow)); // logic for keeping each row at three chunks
     }
+     console.log("=====>>>>>>>", saturday);
+
     res.render("businesses/index", {
       title: "Dealers", //hook
       business: poolPros, //hook
+      closed:saturday[i] === "",
+      flash: message
     });
-    console.log("this is businesses >>>",poolPros[2]);
+   
 });
 
 module.exports = router;

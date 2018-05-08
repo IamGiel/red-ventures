fa055f3fc3287a67b956b671a76e4f8dae849d30
Server <november14giel@gmail.com>
2018-05-07 23:38:23 -0400

    added filter capability in routes

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,126 @@
-var express = require('express');
-var router = express.Router();
-var dealers = require('../data/dealers')
+const express = require("express");
+const router = express.Router();
+const dealers = require("../data/dealers");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
- 
-    var poolPros = []; //were gonna render this array, in our HOOK, as a key in our hbs page
-    var saturday =[];
-    var certifications= [];
-    var chunksPerRow = 3; //just to say, we want each row to contain three cards/container
-    //here we loop through dealers
-    // for (let i = 0; i < dealers.length; i++) {
-    //  saturday.push(dealers[i].data.weekHours.sat);
-    //  certifications.push(dealers[i].data.certifications[i]);
-    // }
-    for (var i = 0; i < dealers.length; i += chunksPerRow) {
-      // i is incremented by three
-      poolPros.push(dealers.slice(i, i + chunksPerRow)); // logic for keeping each row at three chunks
-      certifications.push(dealers[i].data.certifications);
-    }
-     console.log("=====>>>>>>>", dealers.length);
+router.get("/", (req, res, next) => {
+  const poolPros = []; //were gonna render this array, in our HOOK, as a key in our hbs page
+  const saturday = [];
+  const certifications = [];
+  const chunksPerRow = 3; //just to say, we want each row to contain three cards/container
+  for (let i = 0; i < dealers.length; i += chunksPerRow) {
+    // i is incremented by three
+    poolPros.push(dealers.slice(i, i + chunksPerRow)); // logic for keeping each row at three chunks
+    certifications.push(dealers[i].data.certifications);
+  }
+  console.log("=====>>>>>>>", dealers.length);
 
-    res.render("businesses/cards", {
-      title: "Dealers", //hook
-      business: poolPros, //hook
-      certificate: certifications,
-      number: dealers.length
-    });
+  res.render("businesses/cards", {
+    // title: "Dealers", //hook test
+    business: poolPros, //hook
+    certificate: certifications,
+    number: dealers.length
+  });
 });
 
-router.get("/commercial/:id", function(req, res, next) {
-  var commercial = [];
+// render all business with service certification
+router.get("/service", (req, res, next) => {
 
-  var bussinessId = req.params.id; //retrieve id here
-  res.redirect("/commercial");
+  let installationPro = [];
+  for (let i = 0; i < dealers.length; i++) {
+    //if in certifications array
+    //one of the string values match 'Installation Pro'
+    //render this business card
+    const installPro = dealers[i].data.certifications;
+    const wordsToMatch = installPro.join(" ");
+    if (wordsToMatch.indexOf("Service", 0) >= 0) {
+      installationPro.push(dealers[i]);
+    } else {
+      console.log("INDEX: ", wordsToMatch.indexOf("Rental"));
+    }
+  }
+  console.log("BUSINESSES IN THIS LIST: ", installationPro);
+  console.log("LENGTH: ", installationPro.length)
+
+  res.render("businesses/service", {
+    title: "Dealers", //hook test
+    installPro: installationPro,
+    number: installationPro.length
+  });
+});
+
+// render all business with commercial certification
+router.get("/commercial", (req, res, next) => {
+  let installationPro = [];
+  for (let i = 0; i < dealers.length; i++) {
+    //if in certifications array
+    //one of the string values match 'Installation Pro'
+    //render this business card
+    const installPro = dealers[i].data.certifications;
+    const wordsToMatch = installPro.join(" ");
+    if (wordsToMatch.indexOf("Commercial", 0) >= 0) {
+      installationPro.push(dealers[i]);
+    } else {
+      console.log("INDEX: ", wordsToMatch.indexOf("Commercial"));
+    }
+  }
+  console.log("BUSINESSES IN THIS LIST: ", installationPro[0].data.name);
+  console.log("LENGTH: ", installationPro.length);
+
+  res.render("businesses/commercial", {
+    title: "Dealers", //hook test
+    installPro: installationPro,
+    number: installationPro.length
+  });
+});
+
+// render all business with residential certification
+router.get("/residential", (req, res, next) => {
+  let installationPro = [];
+  for (let i = 0; i < dealers.length; i++) {
+    //if in certifications array
+    //one of the string values match 'Installation Pro'
+    //render this business card
+    const installPro = dealers[i].data.certifications;
+    const wordsToMatch = installPro.join(" ");
+    if (wordsToMatch.indexOf("Residential", 0) >= 0) {
+      installationPro.push(dealers[i]);
+    } else {
+      console.log("INDEX: ", wordsToMatch.indexOf("Residential"));
+    }
+  }
+  console.log("BUSINESSES IN THIS LIST: ", installationPro[0].data.name);
+  console.log("LENGTH: ", installationPro.length);
+
+  res.render("businesses/residential", {
+    title: "Dealers", //hook test
+    installPro: installationPro,
+    number: installationPro.length
+  });
+});
+// render all business with installation certification
+router.get("/installation", (req, res, next) => {
+  let installationPro = [];
+  for (let i = 0; i < dealers.length; i++) {
+    //if in certifications array
+    //one of the string values match 'Installation Pro'
+    //render this business card
+    const installPro = dealers[i].data.certifications;
+    const wordsToMatch = installPro.join(" ");
+    if (wordsToMatch.indexOf("Installation", 0) >= 0) {
+      installationPro.push(dealers[i]);
+    } else {
+      console.log("INDEX: ", wordsToMatch.indexOf("Installation"));
+    }
+  }
+  console.log("BUSINESSES IN THIS LIST: ", installationPro[0].data.name);
+  console.log("LENGTH: ", installationPro.length);
+
+  res.render("businesses/installation", {
+    title: "Dealers", //hook test
+    installPro: installationPro,
+    number: installationPro.length
+  });
 });
 
 module.exports = router;

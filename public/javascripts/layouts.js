cf07782259bccb4fa5b4ccbd6349a9be2f37019c
Server <november14giel@gmail.com>
2018-05-11 22:34:10 -0400

    working modal for iphone and laptop screen size

--- public/javascripts/layouts.js
+++ public/javascripts/layouts.js
@@ -1,54 +1,52 @@
-
-  
 // Iphone 6^ toggle button
 // toggle between hiding and showing the dropdown content */
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
 
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    console.log("firing here >>>>>");
-    if (!event.target.matches(".dropbtn")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-          openDropdown.classList.remove("show");
-        }
-      }
-    }
-  };
+var openBtn = document.getElementsByClassName("dropbtn");
+var closeBtn = document.getElementsByClassName("drp-dwn-closer");
+var dropDwnContent = document.getElementsByClassName("dropdown-content");
+$(openBtn).on("click", function() {
+  console.log("opened");
+  $(dropDwnContent).show();
+});
+$(closeBtn).on("click", function() {
+  console.log("closed");
+  $(dropDwnContent).hide();
+});
 
-// modal functionality
-
+// ======= modal for laptop screens=======
 // Get the modal
 var modal = document.getElementById("myModal");
-
+$(modal).hide();
 // Get the button that opens the modal
 var btn = document.getElementById("contact");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function(event) {
-  event.preventDefault()
-  console.log(modal);
-    modal.style.display = "block";
-}
+// When the user clicks on the button, open the modal
+$(btn).on("click", ()=>{
+  console.log('you clicked contact');
+  $(modal).show();
+})
+// ======= modal for iphone6^ screens=======
+// Get the button that opens the modal
+var drpDown = document.getElementById("drop-contact");
+
+// When the user clicks on the button, open the modal
+$(drpDown).on("click", () => {
+  console.log("you clicked contact");
+  $(modal).show();
+});
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function(event) {
-  event.preventDefault()
-    modal.style.display = "none";
-}
+  event.preventDefault();
+  modal.style.display = "none";
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function() {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
